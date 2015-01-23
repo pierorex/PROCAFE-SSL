6d4bf2e87dd802768287b8eb26339910a11822ac
christian <christian@PC>
2015-01-22 22:08:25 -0430

    Tables updated 2.

--- procafe/appProcafe/models.py
+++ procafe/appProcafe/models.py
@@ -29,12 +29,12 @@ class Position(models.Model):
 
 class UserProfile(models.Model):  
     user = models.OneToOneField(User)
-    ID_number = models.IntegerField(primary_key=True)
+    ID_number = models.IntegerField(primary_key=True, verbose_name = "Cédula")
     type = models.CharField(max_length=200, choices=[("ACADEMICO", "Académico"), ("ADMINISTRATIVO", "Administrativo"), ("OBRERO", "Obrero")])
-    job_title = models.CharField(max_length=200)
-    finished_hours = models.IntegerField(default=0)
-    status = models.CharField(max_length=200)
-    is_enabled = models.BooleanField(default=1)
+    position = models.ForeignKey(Position, verbose_name = "Cargo")
+    finished_hours = models.IntegerField(default=0, verbose_name = "Horas finalizadas")
+    status = models.CharField(max_length=200, verbose_name = "Estado")
+    is_enabled = models.BooleanField(default=1, verbose_name = "Habilitado")
     
     def __str__(self):  
         return "%s's profile" % self.user  
@@ -43,12 +43,12 @@ class UserProfile(models.Model):
 
 class Course(models.Model):
     department_ID = models.ForeignKey(Department, editable = False)
-    name = models.CharField(max_length=200)
-    type = models.CharField(max_length=200)
-    init_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    location = models.CharField(max_length=200, choices=[("SARTENEJAS", "Sartenejas"), ("LITORAL", "Litoral")])
-    number_hours = models.IntegerField()
+    name = models.CharField(max_length=200, verbose_name = "Nombre")
+    type = models.CharField(max_length=200, verbose_name = "Tipo")
+    init_date = models.DateTimeField(verbose_name = "Fecha de Inicio")
+    end_date = models.DateTimeField(verbose_name = "Fecha de Fin")
+    location = models.CharField(max_length=200, verbose_name = "Lugar", choices=[("SARTENEJAS", "Sartenejas"), ("LITORAL", "Litoral")])
+    number_hours = models.IntegerField(verbose_name = "Número de Horas")
     
     def __str__(self):
         return str(self.id)
@@ -56,10 +56,10 @@ class Course(models.Model):
 
 
 class Takes(models.Model):
-    user_ID = models.ForeignKey(UserProfile)
-    course_ID = models.ForeignKey(Course)
+    user_ID = models.ForeignKey(UserProfile, editable = False)
+    course_ID = models.ForeignKey(Course, editable = False)
     term = models.CharField(max_length=200, choices=[("SEP-DIC", "Septiembre-Diciembre"), ("ENE-MAR", "Enero-Marzo"), ("ABR-JUL", "Abril-Julio")])
-    year = models.IntegerField()
+    year = models.IntegerField(max_length=4, verbose_name = "Año")
     status = models.CharField(max_length=200, choices=[("APROBADO", "Aprobado"), ("REPROBADO", "Reprobado"), ("INSCRITO", "Inscrito"), ("RETIRADO", "Retirado")])
 
 
