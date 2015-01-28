ba55db38be1e8b8db6173fe6bf4d440898cf07cc
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-28 15:36:44 -0430

    Refactored functions.py/csv_to_UserProfile.

--- procafe/appProcafe/functions.py
+++ procafe/appProcafe/functions.py
@@ -1,21 +1,23 @@
-'''
-Created on Jan 27, 2015
-
-@author: Jose Daniel
-'''
+# -*- coding: utf-8 -*-
+# Functions used all over the appProcafe
 from appProcafe.models import UserProfile, User
-import django
+
 
 def csv_to_UserProfile(file_path):
     with open(file_path, "r") as myfile:
-        data=myfile.read()
+        data = myfile.read()
     for line in data.split("\n"):
         line = line.split(",")
-        if(not UserProfile.objects.filter(ID_number=int(line[0])).exists()):
+        if (not UserProfile.objects.filter(ID_number = int(line[0])).exists()):
             u = User.objects.create_user(line[1],'a@a.com','hola')
             u.first_name=line[1]  
             u.last_name=line[2]
             u.save()
-            p = UserProfile(user_id=u.id,ID_number=int(line[0]),type="Académico",finished_hours=0,status="no se que va aqui",is_enabled=1)
+            p = UserProfile(user_id = u.id,
+                            ID_number = int(line[0]),
+                            type = "Academico",
+                            finished_hours = 0,
+                            status = "no se que va aqui",
+                            is_enabled = 1)
             p.save()
         #else se puede activar el usuario
