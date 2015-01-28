e9b68aa25c70d573b40922e1c18cd48f781f74de
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-28 10:47:36 -0430

    Added a template to handle employees' list upload at templates/users/loadEmployees.html. Worked on loadEmployees view (not finished/tested yet).

--- procafe/appProcafe/views.py
+++ procafe/appProcafe/views.py
@@ -9,4 +9,12 @@ from appProcafe.models import Document
 from appProcafe.forms import DocumentForm
 
 def loadEmployees(request, ):
-    
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+    if form.is_valid():
+        newdoc = Document(docfile = request.FILES['docfile'])
+        newdoc.save()
+        # Redirect to the document list after POST
+        return HttpResponseRedirect(reverse('myproject.myapp.views.list'))
+    else: form = DocumentForm() # A empty, unbound form
