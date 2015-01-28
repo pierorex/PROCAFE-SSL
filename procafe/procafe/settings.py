69a9d195e2ab12a8a3b5764921c35982d861b980
adgalad <carlos.25896@gmail.com>
2015-01-28 02:24:18 -0430

    Pusheando

--- procafe/procafe/settings.py
+++ procafe/procafe/settings.py
@@ -40,7 +40,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'carlos.25896@gmail.com'
-EMAIL_HOST_PASSWORD = 'vikita2533'
+EMAIL_HOST_PASSWORD = '******'
 
 # Templates processors
 TEMPLATE_CONTEXT_PROCESSORS = (
