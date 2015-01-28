814a44f40d7bf0717f250e614521192d0be5347c
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-28 10:35:10 -0430

    Started the loadEmployees view. Added a lot of imports to views.py

--- procafe/appProcafe/views.py
+++ procafe/appProcafe/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from appProcafe.models import Document
+from appProcafe.forms import DocumentForm
 
 def loadEmployees(request, ):
     
