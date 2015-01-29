6328e52659e7ad753c25c44c7558a5783c76ff68
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-28 23:38:47 -0430

    Fixed bug avoiding the login view to show up in host:port/appProcafe/login

--- procafe/appProcafe/urls.py
+++ procafe/appProcafe/urls.py
@@ -6,4 +6,7 @@ from appProcafe import views
 urlpatterns = patterns('', 
     url(r'^loadEmployees/', views.loadEmployees, name='loadEmployees'),
     url(r'^index/', views.index, name='index'),
+    url(r'^signup/', 'appProcafe.views.signup'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
 )
--- procafe/procafe/urls.py
+++ procafe/procafe/urls.py
@@ -6,16 +6,9 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'procafe.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^appProcafe/', include('appProcafe.urls',namespace='appProcafe')),
     url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
-    url(r'^$|index/', 'appProcafe.views.index'),    # delete?
-    url(r'^signup/', 'appProcafe.views.signup'),    # delete?
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header = 'Administración PROCAFE-SSL'
