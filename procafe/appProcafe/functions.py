dfd0a586ca18a22fc73ffc30bef83222ec323b8f
JoseD92 <jose_daniel_d@hotmail.com>
2015-01-30 02:53:51 -0430

    Mejorada funcion csv_to_UserProfile
    ahora es mas robusta y no dara error

--- procafe/appProcafe/functions.py
+++ procafe/appProcafe/functions.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 # Functions used all over appProcafe
 from appProcafe.models import UserProfile, User
+import re
 
 
 def csv_to_UserProfile(file_path):
     with open(file_path, "r") as myfile:
         data = myfile.read()
-    for line in data.split("\n"):
-        line = line.split(",")
-        if (not UserProfile.objects.filter(ID_number = int(line[0])).exists()):
-            u = User.objects.create_user(line[1],'a@a.com','hola')
-            u.first_name = line[1]
-            u.last_name = line[2]
-            u.save()
-            p = UserProfile(user_id = u.id,
-                            ID_number = int(line[0]),
-                            type = "Academico",
-                            finished_hours = 0,
-                            status = "no se que va aqui",
-                            is_enabled = 1)
-            p.save()
-        #else se puede activar el usuario
+    linestipo = re.compile("^[0-9]+,\"?.*?\"?,\"?.*?\"?,\"?.*?\"?,\"?.*?\"?,\"?.*?\"?,\"?.*?\"?$")#si, esto es una expresion regular
+    for line2 in data.split("\n")[1:]:
+        print(line2+" "+str(linestipo.match(line2)))
+        if linestipo.match(line2):
+            line = line2.split(",")
+            if (not UserProfile.objects.filter(ID_number = int(line[0])).exists()):
+                u = User.objects.create_user(line[1],'a@a.com','hola')
+                u.first_name = line[1]
+                u.last_name = line[2]
+                u.save()
+                p = UserProfile(user_id = u.id,
+                                ID_number = int(line[0]),
+                                type = "Academico",
+                                finished_hours = 0,
+                                status = "no se que va aqui",
+                                is_enabled = 1)
+                p.save()
+        else:
+            continue
+        #else se puede activar el usuario si este ya existe
