831e4c6c2ce926dff64c8165d4c60b42b015b104
christian <chris.atg.2x@gmail.com>
2015-02-01 15:14:38 -0430

    Changes in DB.

--- procafe/appProcafe/migrations/0002_auto_20150201_1940.py
+++ procafe/appProcafe/migrations/0002_auto_20150201_1940.py
@@ -14,8 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Document',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
-                ('file', models.FileField(upload_to='documents/%Y/%m/%d')),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('file', models.FileField(upload_to='documents')),
             ],
             options={
             },
@@ -23,84 +23,90 @@ class Migration(migrations.Migration):
         ),
         migrations.AlterModelOptions(
             name='course',
-            options={'verbose_name_plural': 'Cursos', 'verbose_name': 'Curso'},
+            options={'verbose_name': 'Curso', 'verbose_name_plural': 'Cursos'},
         ),
         migrations.AlterModelOptions(
             name='department',
-            options={'verbose_name_plural': 'Departamentos', 'verbose_name': 'Departamento'},
+            options={'verbose_name': 'Departamento', 'verbose_name_plural': 'Departamentos'},
         ),
         migrations.AlterModelOptions(
             name='position',
-            options={'verbose_name_plural': 'Cargos', 'verbose_name': 'Cargo'},
+            options={'verbose_name': 'Cargo', 'verbose_name_plural': 'Cargos'},
         ),
         migrations.AlterModelOptions(
             name='risk',
-            options={'verbose_name_plural': 'Riesgos', 'verbose_name': 'Riesgo'},
+            options={'verbose_name': 'Riesgo', 'verbose_name_plural': 'Riesgos'},
         ),
         migrations.AlterModelOptions(
             name='section',
-            options={'verbose_name_plural': 'Secciones', 'verbose_name': 'Sección'},
+            options={'verbose_name': 'Sección', 'verbose_name_plural': 'Secciones'},
         ),
         migrations.AlterModelOptions(
             name='takes',
-            options={'verbose_name_plural': 'Cursa', 'verbose_name': 'Cursa'},
+            options={'verbose_name': 'Cursa', 'verbose_name_plural': 'Cursa'},
         ),
         migrations.AlterModelOptions(
             name='unit',
-            options={'verbose_name_plural': 'Unidades', 'verbose_name': 'Unidad'},
+            options={'verbose_name': 'Unidad', 'verbose_name_plural': 'Unidades'},
         ),
         migrations.AlterField(
             model_name='course',
             name='department_ID',
             field=models.ForeignKey(verbose_name='Departamento', to='appProcafe.Department'),
-            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='course',
+            name='number_hours',
+            field=models.IntegerField(verbose_name='NAmero de Horas'),
         ),
         migrations.AlterField(
             model_name='department',
             name='name',
             field=models.CharField(verbose_name='Nombre', max_length=200),
-            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='department',
             name='unit_ID',
             field=models.ForeignKey(verbose_name='Unidad superior', default=0, to='appProcafe.Unit'),
-            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='section',
             name='department_ID',
             field=models.ForeignKey(verbose_name='Departamento', to='appProcafe.Department'),
-            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='section',
             name='name',
             field=models.CharField(verbose_name='Nombre', max_length=200),
-            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='takes',
             name='course_ID',
             field=models.ForeignKey(verbose_name='Curso', to='appProcafe.Course'),
-            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='takes',
             name='status',
-            field=models.CharField(choices=[('APROBADO', 'Aprobado'), ('REPROBADO', 'Reprobado'), ('INSCRITO', 'Inscrito'), ('RETIRADO', 'Retirado')], verbose_name='Estado', max_length=200),
-            preserve_default=True,
+            field=models.CharField(verbose_name='Estado', choices=[('APROBADO', 'Aprobado'), ('REPROBADO', 'Reprobado'), ('INSCRITO', 'Inscrito'), ('RETIRADO', 'Retirado')], max_length=200),
         ),
         migrations.AlterField(
             model_name='takes',
             name='term',
-            field=models.CharField(choices=[('SEP-DIC', 'Septiembre-Diciembre'), ('ENE-MAR', 'Enero-Marzo'), ('ABR-JUL', 'Abril-Julio')], verbose_name='Trimestre', max_length=200),
-            preserve_default=True,
+            field=models.CharField(verbose_name='Trimestre', choices=[('SEP-DIC', 'Septiembre-Diciembre'), ('ENE-MAR', 'Enero-Marzo'), ('ABR-JUL', 'Abril-Julio')], max_length=200),
         ),
         migrations.AlterField(
             model_name='takes',
             name='user_ID',
             field=models.ForeignKey(verbose_name='Nombre', to='appProcafe.UserProfile'),
-            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='takes',
+            name='year',
+            field=models.IntegerField(verbose_name='AAo', max_length=4),
+        ),
+        migrations.AlterField(
+            model_name='userprofile',
+            name='type',
+            field=models.CharField(choices=[('ACADEMICO', 'AcadAmico'), ('ADMINISTRATIVO', 'Administrativo'), ('OBRERO', 'Obrero')], max_length=200),
         ),
     ]
