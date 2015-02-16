fb4c2ad514764cce842da29a1770739093af201e
gebh <gebenzecri@gmail.com>
2015-02-16 18:05:38 -0430

    Request form created in forms.py

--- procafe/appProcafe/forms.py
+++ procafe/appProcafe/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from datetime import datetime
 from django.forms.widgets import TextInput
 from django.core.validators import RegexValidator
 
@@ -20,3 +21,15 @@ class UserLogin(forms.Form):
         widgets = {
             'password': forms.PasswordInput(),
         }
+        
+class RequestForm(forms.Form):
+    ID_number = forms.IntegerField(required=True, verbose_name="Cédula", default=0)
+    USB_ID = forms.CharField(required=True,max_length=8, unique=True, validators=[RegexValidator(regex="^[0-9]{2}-[0-9]{5}$", message="El USB-ID debe ser de la forma xx-xxxxx.", code="invalid_usbid")], null=True)
+    firstname = forms.CharField(required=True,max_length=50, verbose_name="Nombre", default="")
+    lastname = forms.CharField(required=True,max_length=50, verbose_name="Apellido", default="")
+    birthday = forms.DateField(required=True,verbose_name="Fecha de Nacimiento", default=datetime.today())
+    paysheet = forms.CharField(required=True,max_length=14, verbose_name="Tipo de Nómina", choices=[("ACADEMICO", "Académico"), ("ADMINISTRATIVO", "Administrativo"), ("OBRERO", "Obrero")], default=None)
+    type = forms.CharField(required=True,max_length=20, choices=[("----", "----")], verbose_name="Tipo de Personal", default=None)
+    location = forms.CharField(required=True,max_length=200, verbose_name="Ubicación de Trabajo", default=None)
+    position = forms.ChoiceField(required=True, verbose_name="Cargo", choices=[], default=None)
+    email = forms.EmailField(required=True,verbose_name="E-mail", default=None)
