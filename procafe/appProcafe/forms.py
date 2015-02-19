ee75d10d389580f282c0dd0915a3bb1296ac8127
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-02-19 18:07:58 -0430

    Forced every field in RequestForm. Refactored some code. Fix: validate user id_number in RequestForm.

--- procafe/appProcafe/forms.py
+++ procafe/appProcafe/forms.py
@@ -4,6 +4,11 @@ from django.core.validators import RegexValidator
 from appProcafe.models import Location, Position, Paysheet, Type
 
 
+cedula_validator =  RegexValidator(
+                        regex="^[1-9][0-9]{0,7}$", 
+                        message="La cédula debe contener entre 1 y 8 caracteres numéricos.")
+cedula_length = 9
+
 class DocumentForm(forms.Form):
     file = forms.FileField (
         label='Seleccione el archivo de nomina en formato .csv:'
@@ -13,16 +18,13 @@ class DocumentForm(forms.Form):
 class UserSignUpForm(forms.Form):    
     id = forms.CharField(required = True,
                     label = "Cédula",
-                    max_length = 10,
-                    validators = [
-                          RegexValidator(
-                                regex = '^[0-9]+$',
-                                message = 'Cedula Invalida.'
-                        )
-                    ])
+                    max_length = cedula_length,
+                    validators = [cedula_validator])
 
 class UserLogin(forms.Form):
-    id = forms.CharField(max_length=8, label = "Cédula", validators=[RegexValidator(regex="^[1-9][0-9]{0,7}$", message="La cédula debe contener a lo sumo 8 caracteres numéricos.", code="invalid_id")])
+    id = forms.CharField(max_length=cedula_length, 
+                         label = "Cédula", 
+                         validators=[cedula_validator])
     password = forms.CharField(required = True, 
                                label = 'Contraseña',
                                widget=forms.PasswordInput())
@@ -34,17 +36,20 @@ class UserLogin(forms.Form):
 
         
 class RequestForm(forms.Form):
-    ID_number = forms.IntegerField(required = False,label="Cédula")
-    USB_ID = forms.CharField(required = False, max_length=8, validators=[RegexValidator(regex="^[0-9]{2}-[0-9]{5}$", message="El USB-ID debe ser de la forma xx-xxxxx.", code="invalid_usbid")])
-    first_name = forms.CharField(required = False,max_length=50, label="Nombre")
-    last_name = forms.CharField(required = False,max_length=50, label="Apellido")
-    birthdate = forms.DateField(required = False,label="Fecha de Nacimiento")
-    paysheet = forms.ChoiceField(required = False, label="Tipo de Nómina",choices=[])
-    type = forms.ChoiceField(required = False, label="Tipo de Personal",choices=[])
-    location = forms.ChoiceField(required = False,label="Ubicación de Trabajo",choices=[])
-    position = forms.ChoiceField(required = False,label="Cargo",choices=[])
-    sex = forms.ChoiceField(required = False,label="Sexo",choices=[("MASCULINO","Masculino"),("FEMENINO","Femenino")])
-    email = forms.EmailField(required = False,label="E-mail")
+    ID_number = forms.CharField(required = True,
+                    label = "Cédula",
+                    max_length = cedula_length,
+                    validators = [cedula_validator])
+    USB_ID = forms.CharField(required = True, max_length=8, validators=[RegexValidator(regex="^[0-9]{2}-[0-9]{5}$", message="El USB-ID debe ser de la forma xx-xxxxx.", code="invalid_usbid")])
+    first_name = forms.CharField(required = True,max_length=50, label="Nombre")
+    last_name = forms.CharField(required = True,max_length=50, label="Apellido")
+    birthdate = forms.DateField(required = True,label="Fecha de Nacimiento")
+    paysheet = forms.ChoiceField(required = True, label="Tipo de Nómina",choices=[])
+    type = forms.ChoiceField(required = True, label="Tipo de Personal",choices=[])
+    location = forms.ChoiceField(required = True,label="Ubicación de Trabajo",choices=[])
+    position = forms.ChoiceField(required = True,label="Cargo",choices=[])
+    sex = forms.ChoiceField(required = True,label="Sexo",choices=[("MASCULINO","Masculino"),("FEMENINO","Femenino")])
+    email = forms.EmailField(required = True,label="E-mail")
     
     def __init__(self, data = None):
         
