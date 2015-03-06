a71f1ff96711aa2d9d9b865187743be22657105c
adgalad <carlos.25896@gmail.com>
2015-03-05 23:49:21 -0430

    debug

--- procafe/procafe/settings.py
+++ procafe/procafe/settings.py
@@ -31,10 +31,10 @@ STATICFILES_DIRS = ( os.path.join(BASE_DIR,'procafe/static/media/'),
 SECRET_KEY = 'e$h4gpt@rv*!gxp5+1+-8%s=w9fa=szcwi6t+vuw5=_i_z^ja_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
